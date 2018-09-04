d4f580dba70ac6c95921ce265a9129e09d906086
xiashengming <xiasem@163.com>
2018-09-04 16:08:04 +0800

    init project

---
+++ app/src/main/java/org/doubleprotein/mathilda/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.doubleprotein.mathilda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
